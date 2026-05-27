3fe15d7d623a841e2f2e43a2d06101d55d51223b
braz9LKDI <67658575+braz9LKDI@users.noreply.github.com>
2026-05-27 18:13:44 -0500

    style(vscode): format symbolic_links.ps1 for consistent indentation

--- vscode/symbolic_links.ps1
+++ vscode/symbolic_links.ps1
@@ -4,13 +4,13 @@ $files = "settings.json", "keybindings.json"
 New-Item -ItemType Directory -Path $codeUserPath -Force | Out-Null
 
 foreach ($name in $files) {
-	$sourcePath = (Resolve-Path (Join-Path $PSScriptRoot $name)).Path
-	$destinationPath = Join-Path $codeUserPath $name
+  $sourcePath = (Resolve-Path (Join-Path $PSScriptRoot $name)).Path
+  $destinationPath = Join-Path $codeUserPath $name
 
-	if (Test-Path -LiteralPath $destinationPath) {
-		Remove-Item -LiteralPath $destinationPath -Force
-	}
+  if (Test-Path -LiteralPath $destinationPath) {
+    Remove-Item -LiteralPath $destinationPath -Force
+  }
 
-	New-Item -ItemType SymbolicLink -Path $destinationPath -Target $sourcePath | Out-Null
-	Write-Host "$destinationPath -> $sourcePath"
+  New-Item -ItemType SymbolicLink -Path $destinationPath -Target $sourcePath | Out-Null
+  Write-Host "$destinationPath -> $sourcePath"
 }
